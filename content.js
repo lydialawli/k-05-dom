cf14c228e2c3eb52bf752abc1ccf748b2bcd1738
Ly <lydialawli@gmail.com>
2019-08-26 14:52:32 +0700

    kata 050502

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
-//050301
-let mainDOM = document.getElementById('main')
+// //050301
+// let mainDOM = document.getElementById('main')
 
-console.log(mainDOM.innerHTML)
-
-
-//050302
-let contentDOM = document.getElementById('content')
-
-contentDOM.innerHTML = 'Main Content'
+// console.log(mainDOM.innerHTML)
 
 
-//050303
-let productsDOM = document.getElementById('products')
+// //050302
+// let contentDOM = document.getElementById('content')
 
-productsDOM.innerHTML = '<div>1</div><div>2</div>'
+// contentDOM.innerHTML = 'Main Content'
+
+
+// //050303
+// let productsDOM = document.getElementById('products')
+
+// productsDOM.innerHTML = '<div>1</div><div>2</div>'
 
 
 //050303
@@ -21,12 +21,10 @@ let productsDOM = document.getElementById('products')
 
 let products = Array.from(document.getElementsByClassName('product'))
 
-
-let a =''
 console.log("products:",products)
 
 products.forEach(e => {
-    a += `<div class="product">${e.innerHTML}</div>`
+    productsDOM.innerHTML += `<div class="product">${e.innerHTML}</div>`
+    e.parentNode.removeChild(e)
 })
 
-productsDOM.innerHTML = a
--- forms.js
+++ forms.js
@@ -1,13 +1,26 @@
 //050501
-let switchOnOff = (element) => {
-    if(element.classList=='on'){
-        element.classList.remove('on')
-        element.classList.add('off')
-        element.innerHTML = 'OFF'
-    }
-    else{
-        element.classList.remove('off')
-        element.classList.add('on')
-        element.innerHTML = 'ON'
-    }
+// let switchOnOff = (element) => {
+//     if(element.classList=='on'){
+//         element.classList.remove('on')
+//         element.classList.add('off')
+//         element.innerHTML = 'OFF'
+//     }
+//     else{
+//         element.classList.remove('off')
+//         element.classList.add('on')
+//         element.innerHTML = 'ON'
+//     }
+// }
+
+
+//050502
+let getNum = () => {
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+    document.getElementById('total').innerHTML = num1 + num2
 }
+
+
+
+
+
