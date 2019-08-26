1a11b83e6a6a83bc27e7cf911668d048b337c004
Ly <lydialawli@gmail.com>
2019-08-26 15:19:59 +0700

    kata 050503

--- forms.js
+++ forms.js
@@ -14,13 +14,31 @@
 
 
 //050502
-let getNum = () => {
-    let num1 = Number(document.getElementById('num1').value)
-    let num2 = Number(document.getElementById('num2').value)
-    document.getElementById('total').innerHTML = num1 + num2
+// let getNum = () => {
+//     let num1 = Number(document.getElementById('num1').value)
+//     let num2 = Number(document.getElementById('num2').value)
+//     document.getElementById('total').innerHTML = num1 + num2
+// }
+
+//050503
+
+let email = ''
+
+let send = (event) => {
+
+    if (email.includes('@')) {
+        document.getElementById('message').innerHTML = "Thank you for signing up"
+        document.getElementById('message').classList.add('success')
+    }
+    else {
+        document.getElementById('message').innerHTML = "Sorry, please provide a valid email address"
+        document.getElementById('message').classList.add('error')
+    }
+
+    event.preventDefault()
 }
 
 
-
-
-
+let getEmail = (e) => {
+    email = e.value
+}
